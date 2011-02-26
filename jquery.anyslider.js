64c9755ae5c4bcffcab2d7efeb84dbdcfd76e93a
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2011-02-26 16:19:07 +0100

    updated to version 1.1

--- jquery.anyslider.js
+++ jquery.anyslider.js
@@ -1,8 +1,8 @@
 /**
- * jQuery AnySlider 1.0.1
+ * jQuery AnySlider 1.1
  * http://jonathanwilsson.com/
  *
- * Copyright 2010 Jonathan Wilsson
+ * Copyright 2011 Jonathan Wilsson
  * Orginal code by Jacob Gube
  * http://sixrevisions.com/tutorials/javascript_tutorial/create-a-slick-and-accessible-slideshow-using-jquery/
  *
@@ -14,76 +14,93 @@
 
 	$.fn.AnySlider = function (options) {
 
-		var defaults = {
-			current_pos : 0,
-			slide_width : 0,
-			slides : $('.slide'),
-			num_slides : 0
+		// Internal vars. Please don't touch
+		var vars = {
+			currentPos: 0,
+			slideWidth: 0,
+			slides: $('.slide'),
+			numSlides: 0
 		};
 
-		function change_slide(caller) {
+		// Default settings
+		var defaults = {
+			showControls: true,
+			keyboardNav: true,
+			prevLabel: 'Previous slide',
+			nextLabel: 'Next slide'
+		};
+
+		function changeSlide(caller) {
 			// See where the user is going
-			if (caller === '39' || caller === 'next') {
-				defaults.current_pos += 1; // Next slide
+			if (caller === 39 || caller === 'next') {
+				vars.currentPos += 1; // Next slide
 			} else {
-				defaults.current_pos -= 1; // Previous slide
+				vars.currentPos -= 1; // Previous slide
 			}
 
 			// See if slide range has been exceeded
-			if (defaults.current_pos < 0) {
-				defaults.current_pos = defaults.num_slides - 1;
-			} else if (defaults.current_pos >= defaults.num_slides) {
-				defaults.current_pos = 0;
+			if (vars.currentPos < 0) {
+				vars.currentPos = vars.numSlides - 1;
+			} else if (vars.currentPos >= vars.numSlides) {
+				vars.currentPos = 0;
 			}
 
 			// Animate the divs
-			$('#slide-inner').animate({'marginLeft' : defaults.slide_width * (-defaults.current_pos)});
+			$('#slide-inner').animate({marginLeft: vars.slideWidth * (-vars.currentPos)});
 		}
 
 		return this.each(function () {
 
 			var $this = $(this);
 
-			defaults.slide_width = $this.width();
-			defaults.num_slides = defaults.slides.length; // Number of slides
+			vars.slideWidth = $this.width();
+			vars.numSlides = vars.slides.length; // Number of slides
+
+			// If the user has supplied options let's merge them with the defaults
+			if (options) {
+				$.extend(defaults, options);
+			}
 
 			// Remove scrollbar
 			$this.css('overflow', 'hidden');
 
 			// Wrap the .slide divs
-			defaults.slides.wrapAll('<div id="slide-inner"></div>').css({'float' : 'left', 'width' : defaults.slide_width});
+			vars.slides.wrapAll('<div id="slide-inner"></div>').css({float: 'left', width: vars.slideWidth});
 
 			// Set #slide-inner to the total width of all slides
-			$('#slide-inner').css('width', defaults.slide_width * defaults.num_slides);
+			$('#slide-inner').css('width', vars.slideWidth * vars.numSlides);
 
 			// Add the arrows
-			$this.prepend('<span class="arrow" id="prev" title="Previous slide">Previous</span>').append('<span class="arrow" id="next" title="Next slide">Next</span>');
+			$this.prepend('<span class="arrow" id="prev" title="' + defaults.prevLabel + '">' + defaults.prevLabel + '</span>').append('<span class="arrow" id="next" title="' + defaults.nextLabel + '">' + defaults.nextLabel + '</span>');
 
-			// Hide arrows control on first load
+			// Hide controls
 			$('.arrow').hide();
 
-			// Show and hide arrows on hover
-			$this.hover(function () {
-				$('.arrow').show();
-			}, function () {
-				$('.arrow').hide();
-			});
+			if (defaults.showControls) {
+				// Show and hide arrows on hover
+				$this.hover(function () {
+					$('.arrow').show();
+				}, function () {
+					$('.arrow').hide();
+				});
+			}
 
 			// Add event listener for click on previous and next buttons
 			$('.arrow').live('click', function () {
-				change_slide($(this).attr('id'));
+				changeSlide($(this).attr('id'));
 			});
 
-			// Add event listener for keypress on left or right arrow
-			$(document).bind('keydown', function (e) {
-				var key = String(e.keyCode); // Fails unless it's converted to a string
+			if (defaults.keyboardNav) {
+				// Add event listener for keypress on left or right arrow
+				$(document).bind('keydown', function (e) {
+					var key = e.keyCode;
 
-				// See if the left or right arrow is pressed
-				if (key === '37' || key === '39') {
-					change_slide(key);
-				}
-			});
-
+					// See if the left or right arrow is pressed
+					if (key === 37 || key === 39) {
+						changeSlide(key);
+					}
+				});
+			}
 		});
 
 	};

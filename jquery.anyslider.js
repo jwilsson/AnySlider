d883259f5495028adfb11e6447a71d75064da2dd
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2011-08-29 10:02:14 +0200

    version 1.2.2

--- jquery.anyslider.js
+++ jquery.anyslider.js
@@ -1,5 +1,5 @@
 /**
- * jQuery AnySlider 1.2.1
+ * jQuery AnySlider 1.2.2
  * http://jonathanwilsson.com/projects/jquery-anyslider/
  *
  * Copyright 2011 Jonathan Wilsson
@@ -9,7 +9,7 @@
  */
 
 (function ($) {
-	var Anyslider = function (elem, options) {	
+	var Anyslider = function (elem, options) {
 		var $elem = $(elem),
 			$arrows,
 			vars = { // Internal vars. Please don't touch
@@ -27,7 +27,7 @@
 				showOnHover: false,
 				speed: 400
 			};
-	
+		
 		function changeSlide(caller) {
 			// See where the user is going
 			if (caller === 39 || caller === "next-arrow") {
@@ -35,30 +35,30 @@
 			} else {
 			    vars.currentPos -= 1; // Previous slide
 			}
-	    
+			
 			// See if slide range has been exceeded
 			if (vars.currentPos < 0) {
 			    vars.currentPos = vars.numSlides - 1;
 			} else if (vars.currentPos >= vars.numSlides) {
 			    vars.currentPos = 0;
 			}
-	    
+			
 			// Animate the divs
 			if (!vars.animating) {
 				vars.animating = true;
 				
-				$elem.find(".slide-inner").animate({marginLeft: vars.slideWidth * (-vars.currentPos)}, vars.speed, function () {
+				$elem.find(".slide-inner").animate({marginLeft: vars.slideWidth * (-vars.currentPos)}, defaults.speed, function () {
 					vars.animating = false;	
 				});
 			}
 			
 		}
-		    
+		
 		// If the user has supplied options let's merge them with the defaults
 		if (options) {
 			$.extend(defaults, options);
 		}
-		    
+		
 		// Number of slides
 		vars.numSlides = vars.slides.length;
 		
@@ -77,7 +77,7 @@
 		if (!defaults.showControls) {
 			$arrows.hide();
 		}
-
+		
 		if (defaults.showOnHover && !defaults.showControls) {
 			// Show and hide arrows on hover
 			$elem.bind("mouseover", function () {
@@ -96,7 +96,7 @@
 			// Add event listener for keypress on left or right arrow
 			$(document).bind("keydown", function (e) {
 				var key = e.keyCode;
-		
+				
 				// See if the left or right arrow is pressed
 				if (key === 37 || key === 39) {
 					changeSlide(key);
@@ -104,21 +104,21 @@
 			});
 		}
 	};
-    
+	
 	$.fn.AnySlider = function (options) {
-
+		
 		return this.each(function () {
 			var $elem = $(this),
 				anyslider;
-		    
+			
 			// Bail if we already have a plugin insatnce for this element
 			if ($elem.data("anyslider")) {
 				return $elem.data("anyslider");
 			}
-		    
+			
 			// Create a new Anyslider object
 			anyslider = new Anyslider(this, options);
-		    
+			
 			// Store the AnySlider object
 			$elem.data("anyslider", anyslider);
 		});
